ea423891ef476b8387263e13b4eb1331c4bce34c
Esraaft <66075276+Esraaft@users.noreply.github.com>
2020-12-24 14:11:22 +0200

    Delete IConnection.java

--- IConnection.java
+++
@@ -1,9 +0,0 @@
-
-public interface IConnection {
-	
-	public void addNotification(NotificationModule newNotificationModule );
-	public void readNotifications();
-	public void removeNotificationByID(int ID);
-	public NotificationModule geteNotificationByID(int ID );
-	public int getSize();
-}
